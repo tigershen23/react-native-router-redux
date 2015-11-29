fccd8ab7edb1cbfac30aa28ee8c40bcda3b2b797
Robert Malko <robmalko@gmail.com>
2015-11-29 10:52:14 -0500

    set back example code to flex

--- example/ReduxRouter/app/components/Master.js
+++ example/ReduxRouter/app/components/Master.js
@@ -20,13 +20,8 @@ export default Master = (backgroundColor = '#F5FCFF') => class extends Component
 var styles = StyleSheet.create({
   container: {
     alignItems: 'center',
-    bottom: 0,
     flex: 1,
     justifyContent: 'center',
-    left: 0,
-    position: 'absolute',
-    right: 0,
-    top: 0,
   },
   image: {
     height: 200,
