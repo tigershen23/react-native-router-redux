c113a64f2284ccdfa380009aeb9ce13755841352
tigershen23 <tigershen23@gmail.com>
2016-02-17 05:34:48 -0800

    Remove reference to deprecated _isReactElement

--- src/NavBar.js
+++ src/NavBar.js
@@ -2,7 +2,7 @@ import NavigationBar from 'react-native-navbar';
 import React, { Component, StyleSheet } from 'react-native';
 
 const leftButton = (props = {}, transitioning) => {
-  if (props.navLeft && props.navLeft._isReactElement) {
+  if (props.navLeft) {
     return props.navLeft;
   }
 
@@ -23,7 +23,7 @@ const leftButton = (props = {}, transitioning) => {
 };
 
 const rightButton = (props = {}) => {
-  if (props.navRight && props.navRight._isReactElement) {
+  if (props.navRight) {
     return props.navRight;
   }
 
